d1ccf218b5f1d2543b236254f5c0bb1824dc02c8
jedinrk <jedi.nrk@gmail.com>
2023-11-04 22:41:07 +0530

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,24 @@ import type { Metadata } from 'next'
 import { Open_Sans, Public_Sans, Playfair_Display } from 'next/font/google'
 import './globals.css'
 
-const openSans = Open_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  //👇 Add variable to our object
-  variable: '--font-opensans',
-})
+// const openSans = Open_Sans({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   //👇 Add variable to our object
+//   variable: '--font-opensans',
+// })
 
 const publicSans = Public_Sans({
   subsets: ['latin'],
   display: 'swap',
-  //👇 Add variable to our object
-  variable: '--font-publicsans',
 })
 
-//👇 Configure the object for our second font
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-playfair',
-})
+// //👇 Configure the object for our second font
+// const playfair = Playfair_Display({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-playfair',
+// })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -35,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${openSans.variable} ${publicSans.variable} ${playfair.variable} font-sans`}>{children}</body>
+      <body className={publicSans.className}>{children}</body>
     </html>
   )
 }
