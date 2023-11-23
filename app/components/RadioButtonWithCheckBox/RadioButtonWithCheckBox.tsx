6e5256dddf9ba15d91b562149444ecf8744edaaa
jedinrk <jedi.nrk@gmail.com>
2023-11-23 13:23:19 +0530

    removed commented codes

--- app/components/RadioButtonWithCheckBox/RadioButtonWithCheckBox.tsx
+++ app/components/RadioButtonWithCheckBox/RadioButtonWithCheckBox.tsx
@@ -28,13 +28,6 @@ function RadioButtonWithCheckBox({
         {label}
       </label>
     </div>
-    // <div
-    //   className={`w-[270px] h-[50px] px-[50px] py-3.5 ${
-    //     isChecked ? "bg-violet-100" : "bg-white"
-    //   } rounded-[20px] border-2 border-dotted border-zinc-500 justify-center items-center gap-2.5 inline-flex`}
-    // >
-    //   <label className="text-slate-900 text-base font-normal">{label}</label>
-    // </div>
   );
 }
 
