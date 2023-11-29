2794f43ce6cbae5c81c9dd02824a04bc11b4f402
jedinrk <jedi.nrk@gmail.com>
2023-11-29 14:03:18 +0530

    Created Animated Check icon

--- app/components/MultiStepForm/UserFormThankYou.tsx
+++ app/components/MultiStepForm/UserFormThankYou.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Image from "next/image";
-import completedSvg from "../../../public/completed.svg";
-
-const CompletedIcon = () => <Image src={completedSvg} alt="Completed Icon" />;
+import AnimCheck from "../AnimCheck/AnimCheck";
 
 function UserFormThankYou() {
+  const [update, setUpdate] = useState(false);
+
   return (
     <div className="pt-[100px] pb-[110px] flex flex-col items-center">
       <div>
-        <CompletedIcon />
+        {/* <AnimCheck/>
+        <CompletedIcon /> */}
+        {update && <AnimCheck />}
       </div>
-      <div className="mt-[10px] text-slate-900 text-2xl font-extrabold">
+      <div className="mt-[10px] text-slate-900 text-2xl font-extrabold" >
         Thank you for answers!
       </div>
     </div>
